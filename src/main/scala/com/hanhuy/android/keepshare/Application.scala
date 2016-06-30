980c58c3c3bfc3cc1d51b1aec32dc784576d1548
Perry Nguyen <pfnguyen@hanhuy.com>
2016-06-29 17:50:35 -0700

    Rename exception logged

--- src/main/scala/com/hanhuy/android/keepshare/Application.scala
+++ src/main/scala/com/hanhuy/android/keepshare/Application.scala
@@ -4,11 +4,12 @@ import org.acra.ACRA
 import org.acra.annotation.ReportsCrashes
 
 object Application {
+  case class LoggedException(msg: String, t: Throwable) extends Exception(msg, t)
   private var _instance: Application = _
   def instance = _instance
 
   def logException(msg: String, t: Throwable) =
-    ACRA.getErrorReporter.handleSilentException(new Exception(msg, t))
+    ACRA.getErrorReporter.handleSilentException(LoggedException(msg, t))
 }
 
 @ReportsCrashes(formUri = "http://hanhuy-acra.appspot.com/api/crashreport")
