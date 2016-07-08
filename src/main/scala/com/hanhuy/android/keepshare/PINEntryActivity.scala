13b350fcf5f54edcd13d455c4382cdfc273fdd9b
Perry Nguyen <pfnguyen@hanhuy.com>
2016-07-08 16:09:09 -0700

    Convert PINEntryActivity to use TypedViewHolder

--- src/main/scala/com/hanhuy/android/keepshare/PINEntryActivity.scala
+++ src/main/scala/com/hanhuy/android/keepshare/PINEntryActivity.scala
@@ -21,17 +21,13 @@ object PINEntryActivity {
   }
 }
 class PINEntryActivity extends AppCompatActivity with TypedFindView with DialogManager {
-  lazy val prompt = findView(TR.pin_prompt)
-  lazy val pinEntry = findView(TR.pin)
-  lazy val ok = findView(TR.pin_ok)
-  lazy val error = findView(TR.pin_error)
   lazy val settings = Settings(this)
   lazy val km = new KeyManager(this, settings)
   lazy val vibrator = this.systemService[Vibrator]
   lazy val fpm = FingerprintManager(this, settings)
   lazy val fpmObs = fpm.authenticate()
-  val keys = List(R.id.pin_0, R.id.pin_1, R.id.pin_2, R.id.pin_3,
-    R.id.pin_4, R.id.pin_5, R.id.pin_6, R.id.pin_7, R.id.pin_8, R.id.pin_9)
+  lazy val keys = List(views.pin_0, views.pin_1, views.pin_2, views.pin_3,
+    views.pin_4, views.pin_5, views.pin_6, views.pin_7, views.pin_8, views.pin_9)
   private[this] var subscription = Option.empty[Sub]
 
   private[this] var pin = ""
@@ -44,9 +40,9 @@ class PINEntryActivity extends AppCompatActivity with TypedFindView with DialogM
   override def onStart() = {
     super.onStart()
     def validatePin() {
-      pinEntry.setText(pin)
+      views.pin.setText(pin)
       vibrator.vibrate(20)
-      ok.setEnabled(pin.length > 0)
+      views.pin_ok.setEnabled(pin.length > 0)
     }
     val failCount = settings.get(Settings.PIN_FAIL_COUNT)
     if (failCount > 0) {
@@ -56,12 +52,12 @@ class PINEntryActivity extends AppCompatActivity with TypedFindView with DialogM
       val now = System.currentTimeMillis
       if (now < unlockTime) {
         val delayTime = unlockTime - now
-        keys.foreach { findViewById(_).setEnabled(false) }
-        error.setVisibility(View.VISIBLE)
-        error.setText("Previous login failed, please wait")
+        keys.foreach(_.setEnabled(false))
+        views.pin_error.setVisibility(View.VISIBLE)
+        views.pin_error.setText("Previous login failed, please wait")
         UiBus.handler.postDelayed(() => {
-          keys.foreach { findViewById(_).setEnabled(true) }
-          error.setVisibility(View.GONE)
+          keys.foreach(_.setEnabled(true))
+          views.pin_error.setVisibility(View.GONE)
         }, delayTime)
       }
     }
@@ -84,24 +80,24 @@ class PINEntryActivity extends AppCompatActivity with TypedFindView with DialogM
           fpm.registerPin(pin)
           finish()
         } else {
-          error.setVisibility(View.VISIBLE)
-          error.setText(R.string.try_again)
-          pinEntry.setText("")
+          views.pin_error.setVisibility(View.VISIBLE)
+          views.pin_error.setText(R.string.try_again)
+          views.pin.setText("")
           pin = ""
           UiBus.handler.removeCallbacks(clearError)
           UiBus.handler.postDelayed(clearError, 1000)
           if (verifyCount > 2) {
             settings.set(Settings.PIN_FAIL_COUNT, settings.get(Settings.PIN_FAIL_COUNT) + 1)
             settings.set(Settings.PIN_FAIL_TIME, System.currentTimeMillis)
-            error.setText(R.string.no_more_tries)
+            views.pin_error.setText(R.string.no_more_tries)
             setResult(Activity.RESULT_CANCELED)
             finish()
           }
         }
       }
       cloudKey.onFailureMain { case e =>
-        error.setVisibility(View.VISIBLE)
-        error.setText(R.string.key_changed_clear_data)
+        views.pin_error.setVisibility(View.VISIBLE)
+        views.pin_error.setText(R.string.key_changed_clear_data)
         Application.logException("cloudKey onFailureMain", e)
       }
 
@@ -137,7 +133,7 @@ class PINEntryActivity extends AppCompatActivity with TypedFindView with DialogM
         case R.id.pin_0    => pin += "0"
           validatePin()
         case R.id.pin_ok   =>
-          ok.setEnabled(false)
+          views.pin_ok.setEnabled(false)
           if (pin.nonEmpty)
             verifyPin()
         case R.id.pin_back =>
@@ -146,33 +142,30 @@ class PINEntryActivity extends AppCompatActivity with TypedFindView with DialogM
       }
     }
 
-    keys ++ List(R.id.pin_ok, R.id.pin_back) foreach {
-      findViewById(_).onClick(onClick)
-    }
+    keys ++ List(views.pin_ok, views.pin_back) foreach(_.onClick(onClick))
     if (fpm.hasFingerprints && allowFingerprint) {
-      val fpicon = findView(TR.fingerprint_icon)
       def fingerprintError(err: CharSequence, sub: Sub): Unit = {
-        fpicon.animate().alpha(0.0f).setListener(BrowseActivity.animationEnd(
-          _ => fpicon.setVisibility(View.GONE))).start()
-        error.setVisibility(View.VISIBLE)
-        error.setText("Fingerprint:  " + err)
+        views.fingerprint_icon.animate().alpha(0.0f).setListener(BrowseActivity.animationEnd(
+          _ => views.fingerprint_icon.setVisibility(View.GONE))).start()
+        views.pin_error.setVisibility(View.VISIBLE)
+        views.pin_error.setText("Fingerprint:  " + err)
         UiBus.handler.removeCallbacks(clearError)
         UiBus.handler.postDelayed(clearError, 5000)
         sub.unsubscribe()
       }
-      fpicon.setVisibility(View.VISIBLE)
+      views.fingerprint_icon.setVisibility(View.VISIBLE)
       import FingerprintManager._
       subscription = Some(fpmObs.subscribe2((r, sub) => r match {
         case FingerprintSuccess(fpin) =>
           sub.unsubscribe()
-          error.setVisibility(View.GONE)
-          pinEntry.setText("xxxxxx")
+          views.pin_error.setVisibility(View.GONE)
+          views.pin.setText("xxxxxx")
           pin = fpin
-          fpicon.animate().rotation(360.0f).setDuration(250).setListener(
+          views.fingerprint_icon.animate().rotation(360.0f).setDuration(250).setListener(
             BrowseActivity.animationEnd(_ => verifyPin())).start()
         case FingerprintFailure(errorString) =>
-          error.setVisibility(View.VISIBLE)
-          error.setText(errorString)
+          views.pin_error.setVisibility(View.VISIBLE)
+          views.pin_error.setText(errorString)
           UiBus.handler.removeCallbacks(clearError)
           UiBus.handler.postDelayed(clearError, 3000)
         case FingerprintException(ex) => fingerprintError(ex.getMessage, sub)
@@ -189,14 +182,15 @@ class PINEntryActivity extends AppCompatActivity with TypedFindView with DialogM
   }
 
   val clearError: Runnable = () => {
-    error.setText("")
+    views.pin_error.setText("")
   }
+
+  lazy val views: TypedViewHolder.pin_setup = TypedViewHolder.setContentView(this, TR.layout.pin_setup)
 
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setTitle(getTitle + getString(R.string.enter_pin_subtitle))
-    setContentView(R.layout.pin_setup)
-    prompt.setText(R.string.verify_pin)
+    views.pin_prompt.setText(R.string.verify_pin)
     setResult(Activity.RESULT_CANCELED)
 
   }
